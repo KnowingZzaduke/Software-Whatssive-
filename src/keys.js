2f2e1b6874b2b0da6aa5b02e53aed43065b396ea
bvr218 <106783311+bvr218@users.noreply.github.com>
2022-11-02 22:54:15 -0500

    commit

--- src/keys.js
+++ src/keys.js
@@ -4,7 +4,7 @@ module.exports = {
         connectionLimit: 10,
         host: 'localhost',
         user: 'root',
-        password: 'H7TI9WYACM5A5KD',
+        password: '',
         database: 'db_links'
     }
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     res.render('index');
 });
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
 
 module.exports = router;
