0f04dce1b5020ebcb4ee11836314476203f2281c
Knowing.Zzaduke <insperb5013@gmail.com>
2022-11-11 15:13:02 -0500

    Commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,19 @@
 const { src, dest, watch } = require("gulp") //Cargamos el módulo de gulp
 const sass = require('gulp-sass')(require('sass'))//Cargamos el plugin de gulp sass
-
+const plumber = require('gulp-plumber');
 //Compilar sass con una tarea de gulp
 function css(done) {
-    src("src/scss/**/*/.scss")//Identificar todos los cambios en todos los archivos
+    src("src/scss/**/*.scss")//Identificar todos los cambios en todos los archivos
+    .pipe(plumber())//Cargar cambios sin importar que se presenten errores
     .pipe(sass())//Compilar los archivos sass
-    .pipe(dest("build/css"));//Almacenar el código sass en un archivo css
+    .pipe(dest("src/public/css"));//Almacenar el código sass en un archivo css
     done();
 }
 
 
 //Watch que escucha por todos los cambios
 function dev(done){
-    watch("src/scss/**/*/.scss", css)//Escuchar por todos los cambios realizados en todos los archivos
+    watch("src/scss/**/*.scss", css)//Escuchar por todos los cambios realizados en todos los archivos
     done();
 }
     
